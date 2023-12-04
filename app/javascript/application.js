db55670cc91c00a167b3f9a90ea417194636e1d6
andredfs <andre1482000@gmail.com>
2023-12-03 22:25:23 -0300

    tailwind no working yet

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "./react/src/index"
+import "stylesheets/tailwind.css";
--- app/javascript/react/src/components/Auth.jsx
+++ app/javascript/react/src/components/Auth.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SignInButton from './SignInButton'; // Update with the correct path to your SignInButton component
 
+import SignInButton from './SignInButton'; // Import your SignInButton component
+
 const YourPageWithSignIn = () => {
   return (
-    <div>
-      <h1>Welcome to Your Page</h1>
-      {/* Use SignInButton */}
-      <SignInButton />
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-8 text-gray-800">Welcome to Your Page</h1>
+      <div className="bg-white shadow-md rounded-lg p-6">
+        {/* Use SignInButton */}
+        <SignInButton />
+      </div>
     </div>
   );
 };
